a7ae56451f8c8b3fbe8aea96f46209a29ba0414d
Stefan Klut <stefanklut12@gmail.com>
2024-05-23 18:01:36 +0200

    init

---
+++ core/trainer.py
@@ -0,0 +1,49 @@
+import pytorch_lightning as pl
+import torch
+from pytorch_lightning.loggers import TensorBoardLogger
+from torch.nn import functional as F
+from torch.utils.data import DataLoader, random_split
+from torchmetrics import Accuracy
+from torchvision import datasets, transforms
+
+
+class ClassificationTrainer(pl.LightningModule):
+    def __init__(self, model, learning_rate=1e-3):
+        super(ClassificationTrainer, self).__init__()
+        self.model = model
+        self.learning_rate = learning_rate
+        self.train_accuracy = Accuracy()
+        self.val_accuracy = Accuracy()
+        self.test_accuracy = Accuracy()
+
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self.model(x)
+        loss = F.cross_entropy(y_hat, y)
+        acc = self.train_accuracy(y_hat, y)
+        self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        self.log("train_acc", acc, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self.model(x)
+        loss = F.cross_entropy(y_hat, y)
+        acc = self.val_accuracy(y_hat, y)
+        self.log("val_loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.log("val_acc", acc, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self.model(x)
+        loss = F.cross_entropy(y_hat, y)
+        acc = self.test_accuracy(y_hat, y)
+        self.log("test_loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.log("test_acc", acc, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.learning_rate)
+        return optimizer
---
+++ main.py
@@ -0,0 +1,27 @@
+import argparse
+
+
+def get_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Main file for Document Separation")
+
+    io_args = parser.add_argument_group("IO")
+    io_args.add_argument("-t", "--train", help="Train input folder/file", nargs="+", action="extend", type=str, required=True)
+    io_args.add_argument(
+        "-v", "--val", help="Validation input folder/file", nargs="+", action="extend", type=str, required=True
+    )
+
+    tmp_args = parser.add_argument_group("tmp files")
+    tmp_args.add_argument("--tmp_dir", help="Temp files folder", type=str, default=None)
+    tmp_args.add_argument("--keep_tmp_dir", action="store_true", help="Don't remove tmp dir after execution")
+
+    args = parser.parse_args()
+    return args
+
+
+def main(args: argparse.Namespace):
+    pass
+
+
+if __name__ == "__main__":
+    args = get_arguments()
+    main(args)
