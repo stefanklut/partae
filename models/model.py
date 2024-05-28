f102f74db09c7b42c85cc6d957f0de3ec1307d18
Stefan Klut <stefanklut12@gmail.com>
2024-05-28 18:02:11 +0200

    Text encoder and first version of separation

--- models/model.py
+++ models/model.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
 from torchvision.models import ResNet50_Weights
+from transformers import RobertaConfig, RobertaModel, RobertaTokenizer
 
 
 class LazyLinearBlock(nn.Module):
@@ -32,7 +33,7 @@ class LinearBlock(nn.Module):
 
 
 class ImageEncoder(nn.Module):
-    def __init__(self, merge_to_batch=False, resize_size=(224, 224)):
+    def __init__(self, merge_to_batch=True, resize_size=(224, 224)):
         super(ImageEncoder, self).__init__()
 
         self.merge_to_batch = merge_to_batch
@@ -77,7 +78,8 @@ class ImageEncoder(nn.Module):
                 image = x[:, i]
 
                 # Encode the individual images per batch
-                self.encode_image(image)  # (B, 512)
+                encoded_image = self.encode_image(image)  # (B, 512)
+                encoded_images_list.append(encoded_image)
 
             # Recombine the encoded images
             encoded_images = torch.stack(encoded_images_list, dim=1)  # (B, N, 512)
@@ -86,23 +88,82 @@ class ImageEncoder(nn.Module):
 
 
 class TextEncoder(nn.Module):
-    def __init__(self):
+    def __init__(self, merge_to_batch=True):
         super(TextEncoder, self).__init__()
-        self.fc = nn.Linear(300, 512)
+
+        self.merge_to_batch = merge_to_batch
+
+        self.tokenizer = RobertaTokenizer.from_pretrained("pdelobelle/robbert-v2-dutch-base")
+        self.roberta = RobertaModel.from_pretrained("pdelobelle/robbert-v2-dutch-base")
+
+        self.fc = nn.Sequential(
+            LazyLinearBlock(512),
+            LinearBlock(512, 512),
+        )
+
+    def encode_text(self, text: list[str]):
+        # Tokenize the text
+        encoded_input = self.tokenizer(text, padding=True, truncation=True, return_tensors="pt")  # (B, S)
+
+        # Encode the text
+        encoded_text = self.roberta(**encoded_input).last_hidden_state  # (B, S, 768)
+        encoded_text = encoded_text[:, 0]  # (B, 768)
+        encoded_text = self.fc(encoded_text)  # (B, 512)
+
+        return encoded_text
+
+    def forward(self, x: list[list[str]]):
+        N = len(x[0])  # Number of documents
+        print(N)
+        if self.merge_to_batch:
+            # Flatten the input
+            batched_x: list[str] = [text for texts in x for text in texts]
+
+            # Encode the text
+            batched_encoded_texts: torch.Tensor = self.encode_text(batched_x)  # (B * N, 512)
+
+            # Chunk the encoded texts back into the original batch size
+            chunked_encoded_texts: tuple[torch.Tensor, ...] = batched_encoded_texts.chunk(N, dim=0)  # N x (B, 512)
+            encoded_texts: torch.Tensor = torch.stack(chunked_encoded_texts, dim=1)  # (B, N, 512)
+
+        else:
+            # Loop through the documents and encode them
+            encoded_texts_list = []
+            for i in range(N):
+                texts = [x[j][i] for j in range(len(x))]
+
+                # Encode the individual documents per batch
+                encoded_text = self.encode_text(texts)  # (B, 512)
+                encoded_texts_list.append(encoded_text)
+
+            # Recombine the encoded documents
+            encoded_texts: torch.Tensor = torch.stack(encoded_texts_list, dim=1)  # (B, N, 512)
+        return encoded_texts
 
 
 class DocumentSeparator(nn.Module):
-    def __init__(self, image_encoder, text_encoder):
+    def __init__(self, image_encoder, text_encoder, output_size=2):
         super(DocumentSeparator, self).__init__()
         self.image_encoder = image_encoder
         self.text_encoder = text_encoder
-        self.fc = nn.Linear(512, 1)
+        self.lstm = nn.LSTM(input_size=1024, hidden_size=512, num_layers=1, batch_first=True, bidirectional=True)
+        self.flatten = nn.Flatten(1, -1)
+        self.fc = nn.Sequential(
+            LazyLinearBlock(1024),
+            LinearBlock(1024, 512),
+            LinearBlock(512, output_size),
+        )
 
     def forward(self, x):
-        image, text = x
+        image = x["image"]
+        text = x["text"]
         image = self.image_encoder(image)
         text = self.text_encoder(text)
-        x = torch.cat([image, text], dim=1)
+
+        # IDEA Add the image height and width to the embedding, but maybe invert them to keep them close to 0
+        x = torch.cat([image, text], dim=2)
+        x, _ = self.lstm(x)
+        x = self.flatten(x)
         x = self.fc(x)
         return x
 
@@ -111,8 +172,17 @@ if __name__ == "__main__":
     image_encoder = ImageEncoder(merge_to_batch=True)
     image_encoder2 = ImageEncoder(merge_to_batch=False)
     images = torch.randn(2, 3, 3, 224, 224)
-    print(images)
     encoded_images = image_encoder(images)
     encoded_images2 = image_encoder2(images)
     print(encoded_images.size())
     print(encoded_images2.size())
+
+    text_encoder = TextEncoder()
+    texts = [["Hello", "world", "Holy"], ["This", "is", "test"]]
+    encoded_texts = text_encoder(texts)
+    print(encoded_texts.size())
+
+    data = {"image": images, "text": texts}
+    document_separator = DocumentSeparator(image_encoder, text_encoder)
+    output = document_separator(data)
+    print(output.size())
